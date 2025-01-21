7fc04ec5543630b7704150e1b5aab39efc44fb4d
mathman14159 <mathman14159@icloud.com>
2025-01-21 13:47:18 -0700

    Level 2

--- Assets/saveData.cs
+++ Assets/saveData.cs
@@ -7,7 +7,6 @@ public class saveData : MonoBehaviour
 {
     public int endHealth;
     public int endScore;
-    public int endBossScore;
     public int bossScore;
     
     // Start is called before the first frame update
@@ -27,22 +26,13 @@ public class saveData : MonoBehaviour
         {
             endHealth = Health.instance.currentHealth;
             endScore = ScoreCounter.instance.currentScore;
-            endBossScore = BossScore.instance.currentBossScore;
-            PlayerPrefs.SetInt("BossScore", endBossScore);
             PlayerPrefs.SetInt("Health", endHealth);
             PlayerPrefs.SetInt("Score", endScore);
-            if (bossScore >= 0 && bossScore < 3)
+            if (bossScore == 0)
             {
                 SceneManager.LoadScene("Boss_Fight");
             }
-            else if (bossScore >= 3 && bossScore <= 15)
-            {
-                SceneManager.LoadScene("Boss_Fight(V2)");
-            }
-            else if(bossScore > 15)
-            {
-                SceneManager.LoadScene("Boss_Fight(V3)");
-            }
+            
 
 
         }
