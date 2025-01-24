6d1c78a9dacdead98109034a8135ccef648b5b05
mathman14159 <mathman14159@icloud.com>
2025-01-24 11:44:44 -0700

    Second Level, Coins, and Pause

---
+++ Assets/pauseScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseScript : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            pause();
+        }
+    }
+
+    public void pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+    }
+}
+
