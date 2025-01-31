317030e27f61a037a6168a53b53ba3ed30407c96
mathman14159 <133553008+mathman14159@users.noreply.github.com>
2025-01-30 22:14:16 -0700

    Shop
    Quick working shop

---
+++ Assets/Shop.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] GameObject ShopMenu;
+    public int arrows;
+    public int coins;
+    public int cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrows = PlayerPrefs.GetInt("Arrows");
+        coins = PlayerPrefs.GetInt("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void BuyArrows()
+    {
+        if (coins > 0)
+        {
+            arrows += 1;
+            coins -= 1;
+            PlayerPrefs.SetInt("Arrows", arrows);
+            PlayerPrefs.SetInt("Score",coins);
+        }
+    }
+}
